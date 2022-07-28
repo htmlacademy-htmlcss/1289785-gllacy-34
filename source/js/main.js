8dfdd8b61177906b4cd882f1478c348f8277e51f
Mariia <64638931+MariiaFish@users.noreply.github.com>
2022-07-28 16:34:39 +0300

    Slider refactoring - 1

--- source/js/main.js
+++ source/js/main.js
@@ -1,32 +1,37 @@
 // Слайдер
 
 //Variables
+const SLIDER_CLASS_LIST_INDEX = 1;
+const BODY_CLASS_LIST_INDEX = 0;
 const CURRENT_BUTTON_CLASS = "slider-pagination-button-current";
-const FIRST_SLIDER_BUTTON = "first-slider-button";
-const SECOND_SLIDER_BUTTON = "second-slider-button";
-const THIRD_SLIDER_BUTTON = "third-slider-button";
 const ACTIVE_CLASS_NAME = "active";
-const PINK_BODY = "slider-body-1";
-const BLUE_BODY = "slider-body-2";
-const YELLOW_BODY = "slider-body-3";
-const BANAN_IMG_SRC = "img/slider/banana.png";
-const STRAWBERRY_JAM_IMG_SRC = "img/slider/strawberry-jam.png";
-const CARAMEL_MARSHMALLOW_IMG_SRC = "img/slider/caramel-marshmallow.png";
-const SLIDER_PHOTOS = [
-  "img/slider/strawberry-jam.png",
-  "img/slider/banana.png",
-  "img/slider/caramel-marshmallow.png",
+
+const bodyColors = {
+  PINK_BODY: "slider-body-1",
+  BLUE_BODY: "slider-body-2",
+  YELLOW_BODY: "slider-body-3",
+};
+
+const sliderImages = {
+  BANAN_IMG_SRC: "img/slider/banana.png",
+  STRAWBERRY_JAM_IMG_SRC: "img/slider/strawberry-jam.png",
+  CARAMEL_MARSHMALLOW_IMG_SRC: "img/slider/caramel-marshmallow.png",
+};
+const DESCRIPTIONS = [
+  "first-slid-description",
+  "second-slid-description",
+  "third-slid-description",
 ];
 
 const slider = document.querySelector(".slider");
-const body = document.querySelector("body");
+const bodyElement = document.querySelector("body");
 const sliderControls = slider.querySelector(".slider-controls");
 const pagonationButtons = sliderControls.querySelectorAll(
   ".slider-pagination-button"
 );
-const mainImage = slider.querySelector(".slider-main-img");
-const secondSliderImage = slider.querySelector(".second-slider-img");
-const thirdSliderImage = slider.querySelector(".third-slider-img");
+const mainImageElement = slider.querySelector(".slider-main-img");
+const secondSliderImageElement = slider.querySelector(".second-slider-img");
+const thirdSliderImageElement = slider.querySelector(".third-slider-img");
 const sliderDescriptionCollection = slider.querySelectorAll(
   ".slider-description-item"
 );
@@ -43,14 +48,28 @@ const checkClassName = (element, className) => {
   }
 };
 
+const getSliderNumber = (element, classListIndex) => {
+  const sliderClassName = element.classList[classListIndex];
+  let sliderNumber = 0;
+  switch (sliderClassName) {
+    case "second-slider-button":
+      sliderNumber = 1;
+      break;
+    case "third-slider-button":
+      sliderNumber = 2;
+      break;
+  }
+  return sliderNumber;
+};
+
 const removeClassNameCollection = (collection, className) => {
   collection.forEach((collectionItem) => {
     removeCLassName(collectionItem, className);
   });
 };
 
-const changeClassName = (element, className) => {
-  const firstClassNeme = element.classList[0];
+const changeClassName = (element, className, classListIndex) => {
+  const firstClassNeme = element.classList[classListIndex];
   element.classList.replace(firstClassNeme, className);
 };
 
@@ -62,33 +81,56 @@ const changeCurrentClassName = (elements, currentElement, className) => {
   addClassName(currentElement, className);
 };
 
-const changeSliderElements = (
-  currentButton,
-  mainImage,
-  secondSliderImage,
-  thirdSliderImage,
-  bodyElement,
-  descriptions
-) => {
-  if (checkClassName(currentButton, FIRST_SLIDER_BUTTON)) {
-    changeImageSrc(mainImage, STRAWBERRY_JAM_IMG_SRC);
-    changeImageSrc(secondSliderImage, BANAN_IMG_SRC);
-    changeImageSrc(thirdSliderImage, CARAMEL_MARSHMALLOW_IMG_SRC);
-    changeClassName(bodyElement, PINK_BODY);
-    changeCurrentClassName(descriptions, descriptions[0], ACTIVE_CLASS_NAME);
-  } else if (checkClassName(currentButton, SECOND_SLIDER_BUTTON)) {
-    changeImageSrc(mainImage, BANAN_IMG_SRC);
-    changeImageSrc(secondSliderImage, CARAMEL_MARSHMALLOW_IMG_SRC);
-    changeImageSrc(thirdSliderImage, STRAWBERRY_JAM_IMG_SRC);
-    changeClassName(bodyElement, BLUE_BODY);
-    changeCurrentClassName(descriptions, descriptions[1], ACTIVE_CLASS_NAME);
-  } else if (checkClassName(currentButton, THIRD_SLIDER_BUTTON)) {
-    changeImageSrc(mainImage, CARAMEL_MARSHMALLOW_IMG_SRC);
-    changeImageSrc(secondSliderImage, BANAN_IMG_SRC);
-    changeImageSrc(thirdSliderImage, STRAWBERRY_JAM_IMG_SRC);
-    changeClassName(bodyElement, YELLOW_BODY);
-    changeCurrentClassName(descriptions, descriptions[2], ACTIVE_CLASS_NAME);
-  }
+const slides = [
+  {
+    slideElements: {
+      firstImage: sliderImages.STRAWBERRY_JAM_IMG_SRC,
+      secondSliderImage: sliderImages.BANAN_IMG_SRC,
+      thirdSliderImage: sliderImages.CARAMEL_MARSHMALLOW_IMG_SRC,
+      bodyColor: bodyColors.PINK_BODY,
+      descriptionIndex: 0,
+    },
+  },
+  {
+    slideElements: {
+      firstImage: sliderImages.BANAN_IMG_SRC,
+      secondSliderImage: sliderImages.CARAMEL_MARSHMALLOW_IMG_SRC,
+      thirdSliderImage: sliderImages.STRAWBERRY_JAM_IMG_SRC,
+      bodyColor: bodyColors.BLUE_BODY,
+      descriptionIndex: 1,
+    },
+  },
+  {
+    slideElements: {
+      firstImage: sliderImages.CARAMEL_MARSHMALLOW_IMG_SRC,
+      secondSliderImage: sliderImages.STRAWBERRY_JAM_IMG_SRC,
+      thirdSliderImage: sliderImages.BANAN_IMG_SRC,
+      bodyColor: bodyColors.YELLOW_BODY,
+      descriptionIndex: 2,
+    },
+  },
+];
+
+const changeSlide = (slideObject) => {
+  const {
+    slideElements: {
+      firstImage,
+      secondSliderImage,
+      thirdSliderImage,
+      bodyColor,
+      descriptionIndex,
+    },
+  } = slideObject;
+
+  changeImageSrc(mainImageElement, firstImage);
+  changeImageSrc(secondSliderImageElement, secondSliderImage);
+  changeImageSrc(thirdSliderImageElement, thirdSliderImage);
+  changeClassName(bodyElement, bodyColor, BODY_CLASS_LIST_INDEX);
+  changeCurrentClassName(
+    sliderDescriptionCollection,
+    sliderDescriptionCollection[descriptionIndex],
+    ACTIVE_CLASS_NAME
+  );
 };
 
 // Hendlers
@@ -97,14 +139,8 @@ const addButtonEventHendler = (buttons, className) => {
     element.addEventListener("click", (evt) => {
       if (!checkClassName(evt.target, CURRENT_BUTTON_CLASS)) {
         changeCurrentClassName(buttons, evt.target, className);
-        changeSliderElements(
-          evt.target,
-          mainImage,
-          secondSliderImage,
-          thirdSliderImage,
-          body,
-          sliderDescriptionCollection
-        );
+        let sliderIndex = getSliderNumber(evt.target, SLIDER_CLASS_LIST_INDEX);
+        changeSlide(slides[sliderIndex]);
       }
     });
   });
